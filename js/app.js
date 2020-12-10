2c0a51f4b52b94f4adc06c4fe53c8da5acb54481
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-10 19:59:01 +0200

    Added startBtnImg

--- js/app.js
+++ js/app.js
@@ -5,12 +5,34 @@ const MAIN = document.querySelector('.main')
 initApp()
 
 function initApp() {
-    addStartGame()
+    addStartGameScr()
 }
 
-function addStartGame() {
+function addStartGameScr() {
+    const startBtnWrapper = new DocumentFragment()
+
+    const startBtnIco = document.createElement('img')
+    startBtnIco.setAttribute('src', '../img/lucky-cat-toy-svgrepo-com.svg')
+    startBtnIco.setAttribute('alt', 'mentor')
+    startBtnIco.classList.add('startBtnIco')
+
     const startBtn = document.createElement('h3')
     startBtn.textContent = 'Start Game'
     startBtn.classList.add('startBtn')
-    MAIN.appendChild(startBtn)
+
+    const startBtnImg = document.createElement('img')
+    startBtnImg.setAttribute('src', '../img/branches-with-leaves-svgrepo-com.svg')
+    startBtnImg.setAttribute('alt', 'branch with leaves')
+    startBtnImg.classList.add('startBtnImg')
+
+    startBtnWrapper.append(startBtnIco, startBtn, startBtnImg)
+    MAIN.append(startBtnWrapper)
+
+    startBtn.addEventListener('click', function() {
+        hideElem()
+    })
+}
+
+function hideElem() {
+
 }
