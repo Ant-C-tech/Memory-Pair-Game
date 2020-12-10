2994f6b950506de364672cb571a4517f0efcafd2
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-10 18:39:12 +0200

    Added startBtn

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const MAIN = document.querySelector('.main')
+
+initApp()
+
+function initApp() {
+    addStartGame()
+}
+
+function addStartGame() {
+    const startBtn = document.createElement('h3')
+    startBtn.textContent = 'Start Game'
+    startBtn.classList.add('startBtn')
+    MAIN.appendChild(startBtn)
+}
