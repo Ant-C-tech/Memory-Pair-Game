4d836b73d5d6b73c0d957e6e4e7486ff469f071e
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-11 18:25:11 +0200

    Hiragana col#1

--- js/hiragana.js
+++ js/hiragana.js
@@ -1,5 +1,65 @@
 'use strict'
 
-const hiragana = []
+const hiragana = [{
+        jap: 'あ',
+        eng: 'a'
+    },
+    {
+        jap: 'か',
+        eng: 'ka'
+    },
+    {
+        jap: 'さ',
+        eng: 'sa'
+    },
+    {
+        jap: 'た',
+        eng: 'ta'
+    },
+    {
+        jap: 'な',
+        eng: 'na'
+    },
+    {
+        jap: 'は',
+        eng: 'ha'
+    },
+    {
+        jap: 'ま',
+        eng: 'ma'
+    },
+    {
+        jap: 'や',
+        eng: 'ya'
+    },
+    {
+        jap: 'ら',
+        eng: 'ra'
+    },
+    {
+        jap: 'わ',
+        eng: 'wa'
+    },
+    {
+        jap: 'が',
+        eng: 'ga'
+    },
+    {
+        jap: 'ざ',
+        eng: 'za'
+    },
+    {
+        jap: 'だ',
+        eng: 'da'
+    },
+    {
+        jap: 'ば',
+        eng: 'ba'
+    },
+    {
+        jap: 'ぱ',
+        eng: 'pa'
+    },
+]
 
 export default hiragana
